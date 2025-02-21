bf05f3ed8fc78e86c9d9221483f6eac4d2c8907c
firkax <157710697+Firkax69@users.noreply.github.com>
2025-02-21 08:39:09 +0000

    feat: index.js output

--- coinChangeRecursive/index.js
+++ coinChangeRecursive/index.js
@@ -4,7 +4,7 @@ console.log(coinChangeRecursive([1,2,5], 11));
 // Output: 3 (5+5+1)
 
 console.log(coinChangeRecursive([2,5], 3));
-// Output: -1 (not possible)
+// Output: not possible to achieve this amount with given coins
 
 console.log(coinChangeRecursive([1], 0));
 // Output: 0 (zero amount needs zero coins)
