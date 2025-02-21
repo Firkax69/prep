40b58a272149b25e66acc3cb4cc8e9087a6b9e23
firkax <157710697+Firkax69@users.noreply.github.com>
2025-02-21 09:32:46 +0000

    update main function explanation

--- coinChangeRecursive/src/coinChangeRecursive.js
+++ coinChangeRecursive/src/coinChangeRecursive.js
@@ -22,3 +22,8 @@ function coinChangeRecursive(coins, amount) {
 }
 
 export default coinChangeRecursive;
+
+// why we use "helper(remaining-coin) + 1" - For example:
+//If remaining = 6 and we use coin 1, the new subproblem is remaining = 5.
+// Now, when we calculate helper(5), if we find that helper(5) requires 2 coins, we need to add 1 more coin (the 1 coin we just used) to make the total number of coins for remaining = 6.
+// Thus, helper(6 - 1) (which is helper(5)) might return 2 coins, and the +1 makes it 3 coins total (1 coin used for remaining = 6 and 2 more coins for the remaining value 5).
